a03e1ff17fe0b0d083aa54f09987af7336dec5b0
Souvik Biswas <biswassouvik284@gmail.com>
2025-12-12 11:59:40 +0530

    feat: show gif in the browser

--- src/gif_generator.ts
+++ src/gif_generator.ts
@@ -18,12 +18,12 @@ interface GiphyResponse {
   data: GifObject[]
 }
 
-type ResponseBody = { url: string; error?: never } | { error: string; url?: never };
 
 interface GifAPIResponse {
   statusCode : number;
+  isBase64Encoded: boolean;
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': string
   };
   body: string
 }
@@ -31,10 +31,16 @@ interface GifAPIResponse {
 const GIF_API_KEY = process.env.GIF_API_KEY;
 const GIF_API_BASE_URL = process.env.GIF_API_BASE_URL;
 
-const createResponse = (respBody: ResponseBody, statusCode: number = 200): GifAPIResponse => {
+const createResponse = (
+  respBody: string, 
+  statusCode: number = 200, 
+  contentType: string = 'application/json', 
+  isBase64Encoded: boolean = false
+): GifAPIResponse => {
   return {
     statusCode,
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': contentType },
+    isBase64Encoded,
     body: JSON.stringify(respBody),
   };
 };
@@ -42,33 +48,42 @@ const createResponse = (respBody: ResponseBody, statusCode: number = 200): GifAP
 export const handler = async (event: Event): Promise<GifAPIResponse> => {
   // TODO implement
   try {
-    if (!GIF_API_KEY || !GIF_API_BASE_URL) return createResponse({ error: 'Missing Environment Variables' }, 400)
+    if (!GIF_API_KEY || !GIF_API_BASE_URL) return createResponse('Missing Environment Variables', 400)
     
-    if (!event.rawPath) return createResponse({ error: 'Missing Path' }, 400)
+    if (!event.rawPath) return createResponse('Missing Path', 400)
     const paths = event.rawPath.split('/').filter(Boolean);
 
-    if (paths.length > 2) return createResponse({ error: 'Invalid path: too many segments' }, 400)
+    if (paths.length > 2) return createResponse('Invalid path: too many segments', 400)
     
-    if (paths[0] !== 'gif-gen') return createResponse({ error: 'Invalid path' }, 400)
-    if ( !paths[1] ) return createResponse({ error: 'Missing Prompt' }, 400)
+    if (paths[0] !== 'gif-gen') return createResponse('Invalid path', 400)
+    if ( !paths[1] ) return createResponse('Missing Prompt', 400)
     
     const prompt = decodeURIComponent(paths[1]);
     const url = `${GIF_API_BASE_URL}?api_key=${GIF_API_KEY}&q=${prompt}&limit=1`;
 
     const resp = await fetch(url);
     if (!resp.ok) {
-      return createResponse({ error: `Giphy API Error: ${resp.statusText}` }, 502);
+      return createResponse(`Giphy API Error: ${resp.statusText}`, 502);
     }
 
     const resp_json: GiphyResponse = await resp.json();
 
     const gif_url = resp_json?.data?.[0]?.images?.original?.url;
     if (!gif_url)
-      return createResponse({ error: 'No GIF found' }, 404)
+      return createResponse('No GIF found', 404)
 
-    return createResponse({ url: gif_url })
+    const gif = await fetch(gif_url)
+
+    if (!gif.ok) {
+      return createResponse('Failed to Download Gif', 502);
+    }
+    const arrayBuffer = await gif.arrayBuffer();
+    const base64 = Buffer.from(arrayBuffer).toString("base64");
+
+    return createResponse(base64, 200, "image/gif", true)
+  
   } catch (err) {
     console.error(err);
-    return createResponse({ error: 'Internal server error: ' + err}, 500)
+    return createResponse('Internal server error: ' + err, 500)
   }
 };
